a842b60309b1c223adaf64343a34fe5285695e65
pckuo95 <pckuo95@gmail.com>
2021-05-12 18:17:12 +0800

    upload saved

--- mbedmidterm02/wifi_mqtt/mqtt_client.py
+++ mbedmidterm02/wifi_mqtt/mqtt_client.py
@@ -1,8 +1,27 @@
+import serial
 import paho.mqtt.client as paho
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev, 9600)
 
+s.write(bytes("\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+
+s.write(bytes("/LEDControl/run 1 1\r", 'UTF-8'))
+line=s.readline() # Read an echo string from mbed terminated with '\n' (putc())
+print(line)
+line=s.readline() # Read an echo string from mbed terminated with '\n' (RPC reply)
+print(line)
+time.sleep(1)
+s.write(bytes("/sel_gesture/run\r", 'UTF-8'))
 # https://os.mbed.com/teams/mqtt/wiki/Using-MQTT#python-client
-
+count_t = 0
 # MQTT broker hosted on local machine
 mqttc = paho.Client()
 
@@ -17,6 +36,8 @@ def on_connect(self, mosq, obj, rc):
 
 def on_message(mosq, obj, msg):
     print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
+    count_t += 1
+    value[count_t] = str(msg.payload)
 
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed OK")
@@ -44,6 +65,28 @@ while num != 5:
     mqttc.loop()
     time.sleep(1.5)
     num += 1
+
+while count_t == 5:
+    time.sleep(5)
+    s.write(bytes("/sel_gesture/run\r", 'UTF-8'))
+    for x in range(0, 5):
+        tilt_value[x]=s.readline() # Read an echo string from B_L4S5I_IOT01A terminated with '\n'
+
+    t = [1,2,3,4,5,6]
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(t,value)
+    ax[0].set_xlabel('Time')
+
+    ax[0].set_ylabel('gesture ID')
+
+    ax[1].plot(t,tilt_value) # plotting the spectrum
+
+    ax[1].set_xlabel('Time')
+
+    ax[1].set_ylabel('tilted!')
+
+    plt.show()
+
 
 # Loop forever, receiving messages
 mqttc.loop_forever()
